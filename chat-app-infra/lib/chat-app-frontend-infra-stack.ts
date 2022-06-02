053474d7ca1333ff27911f32559403b3a1760526
Adam Azmil <adam@compass-interactive.com>
2022-06-02 14:46:46 +0800

    feat(infra): static website infrastructure provisioned

---
+++ chat-app-infra/lib/chat-app-frontend-infra-stack.ts
@@ -0,0 +1,54 @@
+import { Stack, StackProps, RemovalPolicy,CfnOutput } from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
+import * as cloudfront_origins from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class ChatAppFrontendInfraStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+    const cloudfrontOAI = new cloudfront.OriginAccessIdentity(this, 'cloudfront-OAI', {
+      comment: `OAI for ${id}`
+    });   
+    
+    // Create S3 Bucket for our website
+    const siteBucket = new s3.Bucket(this, "SiteBucket", {
+      websiteIndexDocument: 'index.html',
+      publicReadAccess: true,
+      removalPolicy: RemovalPolicy.DESTROY,
+      autoDeleteObjects: true
+    });
+
+    // Grants access to cloudfront service
+    siteBucket.addToResourcePolicy(new iam.PolicyStatement({
+      actions: ['s3:GetObject'],
+      resources: [siteBucket.arnForObjects('*')],
+      principals: [new iam.CanonicalUserPrincipal(cloudfrontOAI.cloudFrontOriginAccessIdentityS3CanonicalUserId)]
+    }));
+    new CfnOutput(this, 'Bucket', { value: siteBucket.bucketName });
+
+    // Cloudfront distribution service
+    const distribution = new cloudfront.Distribution(this, 'SiteDistribution', {
+      defaultRootObject: 'index.html',
+      defaultBehavior: {
+        origin: new cloudfront_origins.S3Origin(siteBucket, { originAccessIdentity: cloudfrontOAI }),
+        compress: true,
+        allowedMethods: cloudfront.AllowedMethods.ALLOW_GET_HEAD_OPTIONS,
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.ALLOW_ALL
+      }
+    });
+    new CfnOutput(this, 'DistributionId', { value: distribution.distributionId });
+    new CfnOutput(this, 'Domain Name', { value: distribution.domainName });
+
+    // Deploy site contents to S3 bucket
+    new s3deploy.BucketDeployment(this, 'DeployWithInvalidation', {
+      sources: [s3deploy.Source.asset('../chat-app/build')],
+      destinationBucket: siteBucket,
+      distribution,
+      distributionPaths: ['/*']
+    })
+  }
+}

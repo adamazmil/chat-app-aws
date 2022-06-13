9f285df0ea3ec2492ea1b999c8ca76f579314d47
Adam Azmil <adam@compass-interactive.com>
2022-06-13 13:28:11 +0800

    feat(api): getguest lambda integration code. Retrieves guest name from database

---
+++ chat-app-infra/lib/lambda/getguest/getguest.js
@@ -0,0 +1,26 @@
+const { DynamoDBClient } = require('@aws-sdk/client-dynamodb')
+const { DynamoDBDocumentClient, GetCommand } = require('@aws-sdk/lib-dynamodb')
+
+const dbClient = new DynamoDBClient({})
+const ddbDocClient = DynamoDBDocumentClient.from(dbClient)
+exports.handler = async (event) => {
+  try {
+    var item = await ddbDocClient.send(
+      new GetCommand({
+        TableName: process.env.TABLE_NAME,
+        Key: {
+          connectionId: event.requestContext.connectionId,
+        }
+      })
+    )
+  } catch (err) {
+    return {
+      statusCode:500
+    }
+  }
+  var payload = await {type:'getGuest',messsage:item.Item.guestName}
+  return {
+    body: JSON.stringify(payload),
+    statusCode: 200,
+  }
+}
